a9aa671b498e2406a94e81f4b16ca965d68fa5b1
1443576 <1443576@50HW012225.SOAM.TCS.com>
2020-04-27 11:13:05 -0500

    Commit inicial con springboot

---
+++ aplicacionJhonDewey/src/main/java/com/example/jhonDewey/JhonDeweyApplication.java
@@ -0,0 +1,13 @@
+package com.example.jhonDewey;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JhonDeweyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JhonDeweyApplication.class, args);
+	}
+
+}
